2e2e8867cd93835c58ef1b2129e9034ca9d15466
Garrett Jackson <garrettjackson43@gmail.com>
2022-10-10 10:58:55 -0400

    schema2

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO departments (department_name, department_id)
+VALUES ("web delevoper"),
+       ("secrity"),
+       ("office"),
+
+INSERT INTO manager (department_id,manager_name)
+VALUES (1, "Garrett Jackson" ),
+       (2, "Burce Wayne" ),
+       (3. "Clark kent"),
+
+    
+       
