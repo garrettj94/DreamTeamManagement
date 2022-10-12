5df6f0afd1af95ea216880ad26e62e95f7e1dc7c
Brian McKenna <brianmckennaman@gmail.com>
2022-10-12 19:15:08 -0400

    added routes files and manager model

---
+++ controler/api/userRoutes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        })
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email }});
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again. '});
+            return;
+        }
+        const correctPassword = await userData.checkPassword(req.body.password);
+
+        if (!correctPassword) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again. '});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.join({ user: userData, message: 'Logged in'})
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
