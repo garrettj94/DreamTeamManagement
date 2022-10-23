45a2caa1fd00cc475c653d6a1706e16dfb874590
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-10-23 16:16:47 -0400

    moved passport strategy to seperate file

--- utlis/index.js
+++ utlis/index.js
@@ -1,9 +1,23 @@
-const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.checkPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+module.exports = router;
