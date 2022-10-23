6e914743dfa27b04bcb64cedb4743030b098296b
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-10-23 17:45:58 -0400

    modified passport middlware

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../models');
-const withAuth = require('../utlis/index');
+const withAuth = require('./api/auth');
 
 router.post('/', withAuth, async (req, res) => {
     try {
